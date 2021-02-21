f39621c1065242497a76d157b2fba3ad4c2f034e
片山凌 <ryotrumpet328@gmail.com>
2021-02-21 23:32:01 +0900

    クラスをいろいろ定義

---
+++ Assets/Scripts/Bingo/Model/BingoNumber.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BingoNumber : MonoBehaviour
+{
+    private int number;
+    private BingoCellStatus cellStatus;
+
+
+}
---
+++ Assets/Scripts/Const/HostPhase.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// ホスト側のフェーズを定義した定数クラス
+/// </summary>
+public class HostPahse
+{
+    public const string SelectNum = "selectnum";
+    public const string PresentQuestion = "presentquestion";
+    public const string PresentAnswer = "presentanswer";
+}
