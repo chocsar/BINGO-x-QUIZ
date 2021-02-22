fd9f5fb4ac2b4e0e8eb27a5e6f338d2d5e577701
mittan1330 <hudabo1330@gmail.com>
2021-02-23 00:17:17 +0900

    addAnim

---
+++ Assets/Resources/Anim/AnimCloud.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Anim
+{
+    public class AnimCloud : MonoBehaviour
+    {
+        [SerializeField] float nextTime;
+        [SerializeField] GameObject CloudSet;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GenerateCloud();
+        }
+
+
+        void GenerateCloud()
+        {
+            Instantiate(CloudSet, this.transform.position, Quaternion.identity, this.gameObject.transform);
+            Invoke("GenerateCloud", nextTime);
+        }
+
+
+    }
+}
---
+++ Assets/Resources/Anim/AnimDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimDestroy : MonoBehaviour
+{
+    [SerializeField] float destroyTime = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyObj", destroyTime);
+    }
+
+    // Update is called once per frame
+    void DestroyObj()
+    {
+        Destroy(this.gameObject);
+    }
+}
