933fee880b4e926605abbca5e741b943a4a23080
mittan1330 <hudabo1330@gmail.com>
2021-02-23 23:12:32 +0900

    アニメーションの修正

--- Assets/Resources/Anim/CS/FadeManager.cs
+++ Assets/Resources/Anim/CS/FadeManager.cs
@@ -1,19 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour
 {
-    [SerializeField] float fadeTime = 1.5f;
+    [SerializeField] float fadeRate = 0.01f;
+    [SerializeField] bool isFadein;
+    [SerializeField] bool isFadeout; //playOnawake
+    [SerializeField] bool thisIsImage;
+    SpriteRenderer spriteRenderer;
+    Image image;
+    float alpha = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (thisIsImage)
+        {
+            image = GetComponent<Image>();
+        }
+        else
+        {
+            spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        }
+        if (isFadein)alpha = 0;
+        if (isFadeout) alpha = 1;
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        if (isFadein)
+        {
+            alpha  += fadeRate;
+            if (alpha > 1)isFadein = false;
+        }
+        if (isFadeout)
+        {
+            alpha -= fadeRate;
+          
+            if (alpha > 1) isFadein = false;
+        }
+
+        if (thisIsImage)
+        {
+            var color = image.color;
+            color.a = alpha;
+            image.color = color;
+        }
+        else if(spriteRenderer != null)
+        {
+            var color = spriteRenderer.color;
+            color.a = alpha;
+            spriteRenderer.color = color;
+        }
     }
 }
