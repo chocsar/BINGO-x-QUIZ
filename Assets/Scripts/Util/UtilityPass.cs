f774342a3e81044ddb6d2d19fe37f65a6d28c6b2
fukuchang <basketplayer21.aki@gmail.com>
2021-02-23 22:54:34 +0900

    fix: FirebaseRESTを使うように修正

---
+++ Assets/Scripts/Util/UtilityPass.cs
@@ -0,0 +1,31 @@
+﻿namespace Utility
+{
+    public static class UtilityPass
+    {
+        private const string PASSWORD_CHARS =
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-";
+
+        public static string GeneratePassword(int length = 20)
+        {
+            var sb = new System.Text.StringBuilder(length);
+            var r = new System.Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int pos = r.Next(PASSWORD_CHARS.Length);
+                char c = PASSWORD_CHARS[pos];
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public static class UtilityFirebase
+    {
+        public static string StringParse(string _data)
+        {
+            return _data.Trim();
+        }
+    }
+}
