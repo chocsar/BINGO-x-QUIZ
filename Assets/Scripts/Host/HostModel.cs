8459ab9102151d6e68fc6380defa355f17a37e69
fukuchang <basketplayer21.aki@gmail.com>
2021-02-25 17:16:46 +0900

    feature: clientのフェーズを取得（鬼の途中です）

--- Assets/Scripts/Host/HostModel.cs
+++ Assets/Scripts/Host/HostModel.cs
@@ -9,11 +9,15 @@ namespace Host
 {
     public class HostModel : MonoBehaviour
     {
+        [SerializeField] private HostFirebaseLoader firebaseLoader;
+
         public IObservable<string> SubmitHostPhaseEvent => hostPhaseSubject;
         public IObservable<int> SubmitHostNumberEvent => hostSubmitNumSubject;
-
+        public IObservable<Dictionary<string, int>> ClientPhaseEvent => loadClientPhaseSubject;
+         
         private Subject<string> hostPhaseSubject = new Subject<string>();
         private Subject<int> hostSubmitNumSubject = new Subject<int>();
+        private Subject<Dictionary<string, int>> loadClientPhaseSubject = new Subject<Dictionary<string, int>>();
 
         private string[] phases = { HostPhase.SelectNum, HostPhase.PresentQuestion, HostPhase.PresentAnswer };
         
@@ -91,10 +95,77 @@ namespace Host
             hostSubmitNumSubject.OnNext(num);
         }
 
+        public void LoadClientPhase(Unit d)
+        {
+            string data = "";
+            DatabaseReference reference = FirebaseDatabase.Instance.GetReference("userphase");
+            reference.GetValueAsync(10, (res) =>
+            {
+                if (res.success)
+                {
+                    Debug.Log("Success fetched data : " + res.data.GetRawJsonValue());
+                    Debug.Log("res : " + res.data.GetRawJsonValue());
+                    data = res.data.GetRawJsonValue();
+                }
+                else
+                {
+                    Debug.Log("Fetch data failed : " + res.message);
+                }
+            });
+
+            var clientData = Utility.UtilityRestJson.JsonPhaseLoad(data);
+            foreach (var item in clientData)
+            {
+                Debug.Log($"{item.Key} : {item.Value}");
+            }
+            loadClientPhaseSubject.OnNext(PhaseCheck(clientData));
+        }
+
         // ビンゴの乱数生成
         private int RandomGenerateNumber()
         {
             return UnityEngine.Random.Range(1, 26);
+        }
+
+        private Dictionary<string, int> PhaseCheck(Dictionary<string, string> dic)
+        {
+            Debug.Log("check");
+            int[] phaseNum = new int[5]; // 1:ready, 2:beforeanswer, 3: answer, 4: afteranswer, 5: open
+            foreach (var data in dic)
+            {
+                Debug.Log("phase : " + data.Value);
+                switch (data.Value)
+                {
+                    case UserBingoPhase.Ready:
+                        phaseNum[0]++;
+                        break;
+                    case UserBingoPhase.BeforeAnswer:
+                        phaseNum[1]++;
+                        break;
+                    case UserBingoPhase.Answer:
+                        phaseNum[2]++;
+                        break;
+                    case UserBingoPhase.AfterAnswer:
+                        phaseNum[3]++;
+                        break;
+                    case UserBingoPhase.Open:
+                        phaseNum[4]++;
+                        break;
+                    default:
+                        phaseNum[4]++;
+                        break;
+                }
+            }
+            Dictionary<string, int> sendData = new Dictionary<string, int>()
+            {
+                { UserBingoPhase.Ready, phaseNum[0]},
+                { UserBingoPhase.BeforeAnswer, phaseNum[1]},
+                { UserBingoPhase.Answer, phaseNum[2]},
+                { UserBingoPhase.AfterAnswer, phaseNum[3]},
+                { UserBingoPhase.Open, phaseNum[4]},
+            };
+
+            return sendData;
         }
     }
 }
